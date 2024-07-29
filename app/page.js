a2ccb9f6c5daa283f50a73e8ca5722de914f3249
Celeste <cnyx7@proton.me>
2024-07-29 13:26:54 -0300

    Added Footer in page.js

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
         <PyramidGenerator />
     <footer className={styles.footer}>
         <a href="https://www.github.com/celes-sb" target="_blank" rel="noopener noreferrer" className={styles.footerLink}>
-          <i className="fab fa-github"></i> Made by Celeste S. Bareiro
+          Made by Celeste S. Bareiro
         </a>
       </footer>
     </main>
