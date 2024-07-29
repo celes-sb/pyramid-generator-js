0e2ac29af40949cf768c8fe5ed943c5057ddbe66
Celeste <cnyx7@proton.me>
2024-07-29 13:26:10 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,6 +6,11 @@ export default function Home() {
     <main className={styles.main}>
       <h1 className={styles.title}>Pyramid Generator</h1>
         <PyramidGenerator />
+    <footer className={styles.footer}>
+        <a href="https://www.github.com/celes-sb" target="_blank" rel="noopener noreferrer" className={styles.footerLink}>
+          <i className="fab fa-github"></i> Made by Celeste S. Bareiro
+        </a>
+      </footer>
     </main>
   );
 }
